34f691e41e558ff20b06646c2b4cf3b905166ad8
Ricardo <recuricardo@gmail.com>
2024-10-02 19:08:01 +0200

    sesiones , examen año pasado y cookies

---
+++ DWES/Archivos 1EVA/ExamenPrueba-Entrega/infoPersona.php
@@ -0,0 +1,26 @@
+ 
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de Alumnos</title>
+     
+</head>
+<body>
+ 
+	<h1> Informacion Personal   </h1>	
+	<p>informacion personal personal personal  personal personal personal  personal personal personal  personal personal personal</p>
+	 	<a href="sobreNosotros.php">
+        <input type="button" value="InformacionPersonal"   id="sobreNosotros" name="sobreNosotros" required><br><br>
+	 </a>
+	 	<a href="index.php">
+        <input type="button" value="inicio"   id="sobreNosotros" name="sobreNosotros" required><br><br>
+	 </a>
+		 
+ <?php
+ echo "Buen equipo";
+ ?>
+ 
+</body>
+</html>
