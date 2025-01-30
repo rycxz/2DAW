f81a632099085c143440b4404713075f3c9a1604
Ricardo <recuricardo@gmail.com>
2025-01-30 20:51:15 +0100

    cosas

--- Interfaces/Preactica02PaginaWEB_Responsive/Original/js/HeaderFooter.js
+++ Interfaces/Preactica02PaginaWEB_Responsive/Original/js/HeaderFooter.js
@@ -1,25 +1,29 @@
 const header = document.querySelector("header");
 const footer = document.querySelector("footer");
 
-header.innerHTML=`   <div class="contenedor_nav">
-    <a href="../index.html">
-        <img src="../img/logo.png" class="logoHeader" alt="Logo">
-    </a>
-</div>
-
-<div class="headerBotones">
-
-    <div class="cajaBusquedad">
-        <input type="text" placeholder="Buscar..." class="entradaBusquedad">
-        <button class="BusquedadBoton">
-            <img src="../img/iconosNav/search_24dp_000000_FILL0_wght400_GRAD0_opsz24.png" alt="Buscar" class="iconoMenu">
-        </button>
+header.innerHTML=`
+<div class="contenedorNav">
+    <div class="contenedorBusquedad">
+        <a href="../index.html">
+            <img src="../img/logo.png" class="logoHeader" alt="Logo de la empresa">
+        </a>
     </div>
-      <a href="../../Practica Pagina Uno Opera - Ingles/index.html" class="botonLenguaje">
-        <img src="../img/iconosNav/idioma.png" alt="Cambiar Idioma" class="iconoMenu">
-    </a>
-</div>
 
+    <div class="headerBotones">
+        <div class="cajaBusquedad">
+            <form action="/buscar" method="get" role="search">
+                <input type="text" placeholder="Buscar..." class="entradaBusquedad" aria-label="Buscar en el sitio">
+                <button type="submit" class="BusquedadBoton" aria-label="Buscar">
+                    <img src="../img/iconosNav/search_24dp_000000_FILL0_wght400_GRAD0_opsz24.png" alt="Icono de búsqueda" class="iconoMenu">
+                </button>
+            </form>
+
+            <a href="../../Practica Pagina Uno Opera - Ingles/index.html" class="botonLenguaje">
+                <img src="../img/iconosNav/idioma.png" alt="Cambiar idioma" class="iconoMenu">
+            </a>
+        </div>
+    </div>
+</div>
 
 
 
